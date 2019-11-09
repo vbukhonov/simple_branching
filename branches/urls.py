406c99d87100e4f487795b38aa248645da49d339
vbukhonov <vladimir.bukhonov@ontarget-group.com>
2019-11-10 00:34:59 +0300

    Change path converter for url "branch-detail".

--- branches/urls.py
+++ branches/urls.py
@@ -4,5 +4,5 @@ from branches.views import BranchListView, BranchDetailView
 
 urlpatterns = [
     path("", BranchListView.as_view(), name="branch-list"),
-    path("<slug:pk>/", BranchDetailView.as_view(), name="branch-detail"),
+    path("<int:pk>/", BranchDetailView.as_view(), name="branch-detail"),
 ]
