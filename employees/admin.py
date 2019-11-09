c1810d00143549f2b6eef72d8f8dff8f8c575561
vbukhonov <vladimir.bukhonov@ontarget-group.com>
2019-11-09 20:31:27 +0300

    Add EmployeeAdmin class and register it.

--- employees/admin.py
+++ employees/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from employees.models import Employee
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_filter = ("first_name", "last_name")
