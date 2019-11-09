d85bd8725429d9b0cdaf9ece1a19b238f0e9aee5
vbukhonov <vladimir.bukhonov@ontarget-group.com>
2019-11-09 17:12:52 +0300

    Fix extra dot for function "upload_facade_to".

--- branches/utils.py
+++ branches/utils.py
@@ -3,7 +3,4 @@ import os
 
 def upload_facade_to(instance, filename):
     filename_base, filename_ext = os.path.splitext(filename)
-    return "{}_facade.{}".format(
-        instance.name,
-        filename_ext.lower(),
-    )
+    return "{}_facade{}".format(instance.name, filename_ext.lower())
