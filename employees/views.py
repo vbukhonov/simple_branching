6203c90b01569411fa1244828690401848717570
vbukhonov <vladimir.bukhonov@ontarget-group.com>
2019-11-10 15:42:18 +0300

    Add ordering of the employees list by some selected fields.

--- employees/views.py
+++ employees/views.py
@@ -11,6 +11,13 @@ class EmployeeListView(ListView):
         new_context = Employee.objects.all()
         if branch_name:
             new_context = new_context.filter(branch__name=branch_name)
+        order_by_fields = self.request.GET.get("order_by_fields", None)
+        if order_by_fields:
+            order_by_fields = order_by_fields.split(",")
+            if set(order_by_fields).issubset(
+                set([field.name for field in Employee._meta.fields])
+            ):
+                new_context = new_context.order_by(*order_by_fields)
         return new_context
 
     def get_context_data(self, *, object_list=None, **kwargs):
@@ -18,6 +25,10 @@ class EmployeeListView(ListView):
             object_list=object_list, **kwargs
         )
         context["branch_name"] = self.request.GET.get("branch_name", None)
+        order_by_fields = self.request.GET.get("order_by_fields", None)
+        if order_by_fields:
+            order_by_fields = order_by_fields.replace(",", ", ")
+        context["order_by_fields"] = order_by_fields
         return context
 
 
